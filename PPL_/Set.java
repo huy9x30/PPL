59c9555cdbf6b8313f49da140b757541a7d54c5c
huynq <huy9x3k@gmail.com>
2018-02-19 20:27:46 +0700

    init

---
+++ PPL_/Set.java
@@ -0,0 +1,22 @@
+
+/**
+ * @overview A class define data type Set
+ * @author huynq
+ */
+public class Set {
+
+    /**
+     *
+     */
+    public Set() {
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String toString() {
+        return "";
+    }
+}
