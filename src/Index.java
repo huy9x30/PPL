9a6a25bbdbc6bd3c70415f87e25966ffd6c5cd4f
huynq <huy9x3k@gmail.com>
2018-02-19 20:19:31 +0700

    Base files

---
+++ src/Index.java
@@ -0,0 +1,27 @@
+
+/**
+ * @overview A program to create PC object and display a report
+ * @author huynq
+ */
+
+public class Index {
+
+    /**
+     * the main procedure
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public void createObject(PC pc) {
+
+    }
+
+    public void displayReport() {
+
+    }
+
+    public void saveReport() {
+
+    }
+}
